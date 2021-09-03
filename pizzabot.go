8d2501fd870d8ead31500c491c34ef9684158178
Gábor Görzsöny <gabor@gorzsony.com>
2021-09-04 00:20:13 +0200

    Added pizza facts

--- pizzabot.go
+++ pizzabot.go
@@ -1,11 +1,22 @@
 package main
 
+import (
+	_ "embed"
+	"strings"
+)
+
+//go:embed pizza_facts.txt
+var pizzaFacts string
+var PizzaFacts = strings.Split(pizzaFacts, "\n")
+
 func NewPizzaBot(token string) *Bot {
 	return Must(NewBot("pizzabot", token)).
 		WithCommands("pizza").
 		WithStickerSet("pizzabot").
+		WithLines(PizzaFacts...).
 		Done().
-		WithKeywords("pizza").
+		WithKeywords("pizza", "pizzák", "pizzát", "pizzákat", "pizzás").
 		WithStickerSet("pizzabox").
+		WithLines(PizzaFacts...).
 		Done()
 }
